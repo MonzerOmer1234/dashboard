ab9715c00537d3bb7ea6bde2861732410ed2e07b
MonzerOmer1234 <monzeromer048@gmail.com>
2024-08-28 19:49:44 +0200

    add

---
+++ assets/js/test.js
@@ -0,0 +1,50 @@
+
+
+
+
+// let farmsList = document.querySelector('.rich-list')
+
+
+
+
+
+
+
+
+
+// const result = fetch('http://127.0.0.1:8000/api/farms').then(res => {
+//  let data = res.json();
+
+//  return data;
+// }).then (data => {
+//     let farms = data.farms;
+
+//     console.log(farms)
+//     for(let i = 0 ; i < farms.length ; i++){
+//         let farm = document.createElement('li') 
+//         farm.className = 'rich-list-item'
+//         let farmName  = document.createElement('h6')
+//         farmName.innerHTML = `Name : ${farms[i].name}`
+//         let farmDescription = document.createElement('p')
+//         farmDescription.innerHTML = `Description : ${farms[i].description}`
+//         let farmType = document.createElement('p')
+//         farmType.innerHTML = `Type : ${farms[i].crop_type}`
+
+//         farm.appendChild(farmName)
+//         farm.appendChild(farmDescription)
+//         farm.appendChild(farmType)
+
+//         farmsList.appendChild(farm)
+
+
+
+
+//     }
+// })
+
+
+
+
+
+
+
